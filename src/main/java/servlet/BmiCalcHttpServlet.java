befb04e257bce93410be6897750c9416a502151f
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-09-25 12:15:32 +0800

    Add files via upload

--- src/main/java/servlet/BmiCalcHttpServlet.java
+++ src/main/java/servlet/BmiCalcHttpServlet.java
@@ -10,19 +10,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import service.BmiService;
+
 @WebServlet(value = {"/servlet/bmicalc"})
 public class BmiCalcHttpServlet extends HttpServlet {
-
+	
+	private BmiService bmiService = new BmiService();
+	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// 取得參數資料
 		String height = req.getParameter("height"); // 取得表單 height 欄位的資料
 		String weight = req.getParameter("weight"); // 取得表單 weight 欄位的資料
-		// 轉型
-		double h = Double.parseDouble(height);
-		double w = Double.parseDouble(weight);
-		// 計算 bmi
-		double bmi = w / Math.pow(h/100, 2);
+		
+		// 取得 bmi 結果
+		double bmi = bmiService.getBmi(height, weight);
 		
 		//---------------------------------------------------------------------
 		// 透過 jsp 渲染資料
