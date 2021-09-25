c239c36ce21f8263d79214708517b3f30ed03568
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-09-25 11:59:03 +0800

    Add files via upload

--- src/main/java/servlet/BmiCalcHttpServlet.java
+++ src/main/java/servlet/BmiCalcHttpServlet.java
@@ -3,6 +3,7 @@ package servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,8 +24,14 @@ public class BmiCalcHttpServlet extends HttpServlet {
 		// 計算 bmi
 		double bmi = w / Math.pow(h/100, 2);
 		
-		PrintWriter out = resp.getWriter();
-		out.print(bmi);
+		//---------------------------------------------------------------------
+		// 透過 jsp 渲染資料
+		// 1. 建立分派器(並指定 jsp)
+		// 2. 建立要送資料
+		// 3. 分派方式 forward, include
+		RequestDispatcher rd = req.getRequestDispatcher("/form/bmi_result.jsp");
+		req.setAttribute("bmi", bmi); // 建立一個 request 變數名叫 bmi 裡面放入 bmi 的計算結果
+		rd.forward(req, resp); // 重新導向
 	}
 	
 }
