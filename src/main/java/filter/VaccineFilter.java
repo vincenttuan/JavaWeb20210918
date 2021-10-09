e65d81db88e1d5f016bcb752e6ed66d6ccb6fd44
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-10-09 14:17:58 +0800

    Add files via upload

---
+++ src/main/java/filter/VaccineFilter.java
@@ -0,0 +1,15 @@
+package filter;
+
+import java.util.Arrays;
+import java.util.List;
+
+// 請實作一個 VaccineFilter 可以過濾正確的名稱
+// 若不正確則印出 "Vaccine name Error"
+
+public class VaccineFilter {
+	// 合法疫苗名稱
+	private List<String> vaccineNames = Arrays.asList("AZ", "BNT", "MVC");
+	
+	
+	
+}
